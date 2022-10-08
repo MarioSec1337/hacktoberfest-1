c6059a3fee5a77f32fab2cbf428e06223205b996
MarioSec1337 <97659983+MarioSec1337@users.noreply.github.com>
2022-10-08 22:33:30 +0800

    Create helloworldmarz.java

---
+++ scripts/helloworldmarz.java
@@ -0,0 +1,11 @@
+// A modified "hello world" program
+
+class MyClass {
+    public static void main(String[ ] args) {
+        System.out.println("Hello Friends,");
+        System.out.println("");
+        System.out.println("this is the first Java Code of the 'For Beginners' page. We are courious about your first programs!");
+        System.out.println("");
+        System.out.println("Coding is fun!!!");
+    }
+}
